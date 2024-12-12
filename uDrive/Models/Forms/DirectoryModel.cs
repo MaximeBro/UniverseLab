bfdbbc05ac119347198aec7cf3e65184ceeabe8d
MaximeBro <maxime.brochard@bearstudio.fr>
2024-12-12 14:16:00 +0100

    WIP forms validation

--- uDrive/Models/Forms/DirectoryModel.cs
+++ uDrive/Models/Forms/DirectoryModel.cs
@@ -5,6 +5,8 @@ namespace uDrive.Models.Forms;
 public class DirectoryModel
 {
     [Required(ErrorMessage = "Ce champ est requis")]
-    [MinLength(3)]
+    [RegularExpression(@"^[a-zA-Z0-9àâäéèêëîïôöùûüç\s._-]+$", 
+        ErrorMessage = "Le nom contient des caractères interdits (\"/\", \"\\\", \":\", \"$\", \"$\", \"<\", \">\", \"?\", \"|\").")]
+    [MinLength(3, ErrorMessage = "Le nom doit être composé de 3 caractères au minimum.")]
     public string Name { get; set; } = string.Empty;
 }
--- uDrive/Models/Forms/FileModel.cs
+++ uDrive/Models/Forms/FileModel.cs
@@ -1,10 +1,46 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 
 namespace uDrive.Models.Forms;
 
 public class FileModel
 {
-    [Required(ErrorMessage = "Ce champ est requis")]
-    [MinLength(3)]
+    [ValidateFileName]
     public string Name { get; set; } = string.Empty;
+}
+
+sealed class ValidateFileName : ValidationAttribute
+{
+    public override bool IsValid(object? value)
+    {
+        string? fileName = value as string;
+
+        if (string.IsNullOrEmpty(fileName))
+        {
+            ErrorMessage += "Le nom est requis. " + Environment.NewLine;
+            return false;
+        }
+        
+        if (fileName.Length < 3)
+        {
+            ErrorMessage += "Le nom doit être composé de 3 caractères au minimum. " + Environment.NewLine;
+            return false;
+        }
+        
+        string invalidCharsPattern = @"[^a-zA-Z0-9àâäéèêëîïôöùûüç\s._-]";
+        if (Regex.IsMatch(fileName, invalidCharsPattern))
+        {
+            ErrorMessage += "Le nom contient des caractères interdits (\"/\", \"\\\", \":\", \"$\", \"$\", \"<\", \">\", \"?\", \"|\"). " + Environment.NewLine;
+            return false;
+        }
+        
+        string extensionPattern = @"\.[a-zA-Z0-9]+$";
+        if (!Regex.IsMatch(fileName, extensionPattern))
+        {
+            ErrorMessage = "Le fichier doit avoir une extension valide. ";
+            return false;
+        }
+
+        return true;
+    }
 }
